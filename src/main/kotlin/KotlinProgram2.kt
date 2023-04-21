1803206818bc4fbd47584a3dc3a9cb0b07461391
bernardPaddilla <mrmillionairegold@gmail.com>
2023-04-22 04:17:01 +0800

    kotlin Program 2

---
+++ src/main/kotlin/KotlinProgram2.kt
@@ -0,0 +1,15 @@
+fun main () {
+    var name: String = "John Weak"
+    var age:Int = 36
+    var salary:Double = 10000.0
+    var skillLevel:Char= 'S'
+    var isActive:Boolean=true
+
+
+    println("your name is $name")
+    println("His age is $age")
+    println("His Skill Level is $skillLevel")
+    println("He is currently active? $isActive")
+
+
+}
