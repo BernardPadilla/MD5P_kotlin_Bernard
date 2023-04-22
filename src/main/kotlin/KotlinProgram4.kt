e8a15f80199c7e3c89ce63fcfdf5ef03a1e629fb
bernardPaddilla <mrmillionairegold@gmail.com>
2023-04-23 02:14:47 +0800

    april23

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,24 @@
+fun main (){
+
+    //arithmetic operator
+    println("This is an example of Addition : ${2+5}")
+    // Store in a variable
+    var sum : Int = 5+8
+    var difference : Int = 5-8
+    var product : Int = 5*8
+    var quotaient : Int = 5/8
+    var modulo : Int = 5%8
+
+    // output
+    println(" The sum is $sum")
+    println(" The difference is $difference")
+    println(" The product is $product")
+    println(" The quotient is $quotaient")
+    println(" The modulo is $modulo")
+
+    // 45 +46/2*10
+
+    var computation = (45+36)/(2*10)
+    println("the result of the computation is $computation")
+
+}
---
+++ src/main/kotlin/KotlinProgram6.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    // logical operators
+    var andOperator = (6>1) && (5>2) // only be tru if both statement is true
+    var orOperator = (1>5)||(6>3) // will be false if both statement is false
+    var notOperator = !(5==5) //invert the result
+
+    println(andOperator)
+    println(orOperator)
+    println(notOperator)
+}
